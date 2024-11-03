e26e7fbc7394cc2f6a79df4b36f885cea4a69faa
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-11-03 18:36:26 -0300

    subindo solucao

---
+++ src/Components/Input/styles.js
@@ -0,0 +1,31 @@
+import { styled } from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display: flex;
+        align-items: flex-end;
+        flex-direction: column;
+        padding: 0 10px;
+
+
+        font-size: 24px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: #FFFFFF;
+    }
+`
